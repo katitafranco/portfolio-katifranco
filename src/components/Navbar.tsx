a41cf02a57e5e8492f8eda56f7a0028f22d1ae37
katiuska.franco <kati.franco@gmail.com>
2025-01-09 12:47:44 -0500

    Add functionality close menu, css, decoration button multilanguage, add logo in  navbar movil

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-scroll";
+import Logo from "./Logo";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -20,9 +21,13 @@ const Navbar = () => {
     { href: "testimonials", label: t("navbar.testimonials") },
   ];
 
+  const handleLinkClick = () => {
+    setMenuOpen(false); // Cierra el menú al hacer clic en un enlace
+  };
+
   return (
     <nav className="w-full bg-white ">
-      <div className="max-w-6xl mx-auto flex justify-between items-center p-4">
+      <div className="max-w-6xl mx-auto flex justify-between items-center p-2">
         {/* Menu para desktop (centrado) */}
         <div className="hidden lg:flex flex-grow justify-center space-x-6">
           {links.map((link) => (
@@ -33,8 +38,9 @@ const Navbar = () => {
               duration={500}
               spy={true}
               offset={-80} // Ajusta si tienes un header fijo
-              activeClass="text-purple-600 font-bold"
+              activeClass="text-purple font-bold"
               className="text-gray-700 hover:text-blue-600 transition cursor-pointer"
+              onClick={handleLinkClick} // para cerrar el menu cuando ya seleccione una opcion
             >
               {link.label}
             </Link>
@@ -43,8 +49,8 @@ const Navbar = () => {
 
         {/* Botón Contact Me y Language Switcher a la derecha */}
         <div className="hidden lg:flex items-center space-x-4">
-          <button className="border border-purple-600 text-purple-600 px-4 py-2 rounded-sm hover:bg-purple-600 hover:text-white transition">
-            <a className="text-purple-600 hover:text-blue-800" href="#contact">
+          <button className="border border-purple-600 text-purple-600 px-4 py-2 rounded-sm hover:bg-blue-500 hover:text-white transition">
+            <a className="text-purple-600 hover:text-white" href="#contact">
               {t("navbar.contactMe")}
             </a>
           </button>
@@ -53,35 +59,61 @@ const Navbar = () => {
           <div className="hidden lg:flex space-x-2 ">
             <button
               onClick={() => changeLanguage("en")}
-              className="text-gray-700 text-sm hover:text-blue-600 transition"
+              className={`text-gray-800 text-sm font-bold hover:text-blue-600 transition ${
+                i18n.language === "en" ? "text-purple-600 underline" : ""
+              }`}
               aria-label="Change language to English"
             >
               EN
             </button>
             <button
               onClick={() => changeLanguage("es")}
-              className="text-gray-700 text-sm hover:text-blue-600 transition"
+              className={`text-gray-800 text-sm font-bold hover:text-blue-600 transition ${
+                i18n.language === "es" ? "text-purple-600 underline" : ""
+              }`}
               aria-label="Cambiar idioma a Español"
             >
               ES
             </button>
           </div>
         </div>
-
-        {/* Mobile Menu Toggle */}
-        <button
-          className="lg:hidden text-gray-700 text-2xl"
-          onClick={() => setMenuOpen(!menuOpen)}
-          aria-label="Toggle menu"
-          aria-expanded={menuOpen ? "true" : "false"}
-        >
-          ☰
-        </button>
       </div>
 
       {/* Mobile Menu */}
+      <div className=" flex justify-between items-center mx-4 lg:hidden ">
+        {/* Mobile Menu Toggle */}
+        <Logo />
+        <div className="flex items-center ">
+          <button
+            className="lg:hidden text-gray-700 text-2xl  mx-4"
+            onClick={() => setMenuOpen(!menuOpen)}
+            aria-label="Toggle menu"
+            aria-expanded={menuOpen ? "true" : "false"}
+          >
+            ☰
+          </button>
+
+          {/* Language Switcher in Mobile */}
+          <button
+            onClick={() => changeLanguage("en")}
+            className={`text-gray-800 text-sm font-bold  mx-1 hover:text-blue-600 transition ${
+              i18n.language === "en" ? "text-purple-600 underline" : ""
+            }`}
+          >
+            EN
+          </button>
+          <button
+            onClick={() => changeLanguage("es")}
+            className={`text-gray-800 text-sm font-bold hover:text-blue-600 transition ${
+              i18n.language === "es" ? "text-purple-600 underline" : ""
+            }`}
+          >
+            ES
+          </button>
+        </div>
+      </div>
       {menuOpen && (
-        <div className="lg:hidden bg-gray-50 p-4">
+        <div className="flex flex-col justify-center items-center lg:hidden bg-gray-50 p-4">
           {links.map((link) => (
             <Link
               key={link.href}
@@ -92,26 +124,11 @@ const Navbar = () => {
               offset={-80} // Ajusta si tienes un header fijo
               activeClass="text-purple-600 font-bold"
               className="block py-2 text-gray-700 hover:text-blue-600 transition"
+              onClick={handleLinkClick} // Cierra el menú al hacer clic en un enlace
             >
               {link.label}
             </Link>
           ))}
-
-          <div className="flex flex-col space-y-2 mt-4">
-            {/* Language Switcher in Mobile */}
-            <button
-              onClick={() => changeLanguage("en")}
-              className="text-gray-700 text-sm hover:text-blue-600 transition"
-            >
-              EN
-            </button>
-            <button
-              onClick={() => changeLanguage("es")}
-              className="text-gray-700 text-sm hover:text-blue-600 transition"
-            >
-              ES
-            </button>
-          </div>
 
           {/* Contact Button Mobile */}
           <button className="border-purple-600 bg-transparent text-white w-full py-2 rounded-lg shadow mt-4 hover:bg-blue-800 transition">
