2a484d51eb10d24140fd978b845ce16560c9438e
katiuska.franco <kati.franco@gmail.com>
2025-01-11 15:59:14 -0500

    Add content in footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,16 +1,68 @@
+// components/Footer.tsx
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
 const Footer = () => {
   return (
-    <section
-      id="footer"
-      className=" flex flex-col justify-center items-center bg-gradient-to-r from-blue-500 to-blue-800 text-white"
-    >
-      <h1 className="text-4xl md:text-6xl font-bold mb-4">
-        I create web and mobile designs
-      </h1>
-      <button className="bg-white text-blue-600 px-6 py-3 rounded-lg shadow hover:bg-gray-100 transition">
-        Get in Touch
-      </button>
-    </section>
+    <footer className="bg-lightGray text-gray-700 py-4">
+      <div className="container mx-auto px-4">
+        <div className="flex justify-center space-x-8">
+          {/* GitHub */}
+          <a
+            href="https://github.com/katitafranco"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex flex-col items-center hover:text-purple transition-all"
+          >
+            <FontAwesomeIcon icon={faGithub} size="2x" />
+            <p className="text-xs mt-2">GitHub</p>
+          </a>
+
+          {/* LinkedIn */}
+          <a
+            href="https://www.linkedin.com/in/katiuska-franco-t/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex flex-col items-center hover:text-purple transition-all"
+          >
+            <FontAwesomeIcon icon={faLinkedin} size="2x" />
+            <p className="text-xs mt-2">LinkedIn</p>
+          </a>
+
+          {/* Email */}
+          <a
+            href="mailto:kati.franco@gmail.com"
+            className="flex flex-col items-center hover:text-purple transition-all"
+          >
+            <FontAwesomeIcon icon={faEnvelope} size="2x" />
+            <p className="text-xs mt-2">Email</p>
+          </a>
+        </div>
+        <p>
+          &copy; {new Date().getFullYear()} Katiuska Franco. All rights
+          reserved.
+        </p>
+        <p className="text-xs">
+          Image credits: Foto de{" "}
+          <a
+            className="hover:text-white"
+            href="https://unsplash.com/es/@ngeshlew?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+            target="_blank"
+          >
+            Lewis Kang'ethe Ngugi
+          </a>{" "}
+          en{" "}
+          <a
+            className="hover:text-white"
+            href="https://unsplash.com/es/fotos/computadora-portatil-negra-encendida-f5pTwLHCsAg?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+            target="_blank"
+          >
+            Unsplash
+          </a>
+        </p>
+      </div>
+    </footer>
   );
 };
 
