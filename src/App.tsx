bea80783becb8a767f2062f93d1d8c0560b1d344
katiuska.franco <kati.franco@gmail.com>
2025-01-11 15:51:46 -0500

    Add library react-slick and slick-carousel, fontawesome , react-icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import Footer from "./sections/Footer";
-
 import Header from "./sections/Header";
 import Hero from "./sections/Hero"; // Importa las otras secciones
 import MyExpertise from "./sections/MyExpertise";
 import AboutMe from "./sections/AboutMe";
 import Portfolio from "./sections/Portfolio";
 import Testimonials from "./sections/Testimonials";
-import ContactForm from "./sections/ContactForm";
+import ContactForm from "./sections/Contact";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const App = () => {
   return (
